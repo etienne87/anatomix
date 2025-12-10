72f32c58e10bb94b1905a75102a89b276096b4a2
eperot <etienne.perot@gehealthcare.com>
2025-12-10 11:52:35 +0100

    push data preproc script

---
+++ anatomix/segmentation/data_preproc.py
@@ -0,0 +1,144 @@
+import os
+import re
+import tqdm
+import json
+from glob import glob
+
+import monai
+from monai.transforms import (
+    Compose,
+    LoadImaged,
+    Orientationd,
+    Spacingd,
+    EnsureChannelFirstd,
+    EnsureTyped,
+    SaveImaged
+)
+
+from plot_utils import viz_mid_slices
+
+
+def find_and_sort_files(basedir):
+    def natural_sort_key(s):
+        """Sort strings containing numbers in natural order"""
+        return [int(text) if text.isdigit() else text.lower()
+                for text in re.split('([0-9]+)', s)]
+
+    # Load all training image and segmentation paths
+    trimages = sorted(
+        glob(
+            os.path.join(basedir, './imagesTr/*.nii.gz'),
+        ),
+        key=natural_sort_key
+    )
+    trsegs = sorted(
+        glob(
+            os.path.join(basedir, './labelsTr/*.nii.gz'),
+        ),
+        key=natural_sort_key
+    )
+    return trimages, trsegs
+
+
+
+def save_json(dataset_path, labels, num):
+    data_dict = {
+        'channel_names':{
+            '0': 'NoNorm'
+        },
+        'labels': labels,
+        "numTraining": num,
+        "file_ending": ".nii.gz"
+    }
+    with open(os.path.join(dataset_path, 'dataset.json'), 'w') as f:
+        json.dump(data_dict, f)
+
+
+
+def preproc(dataset, out_dir, res=[3,1.5,1.5]):
+
+    images, segs = find_and_sort_files(dataset)
+
+    val_files = [
+        {"image": img, "label": seg} for img, seg in zip(images, segs)
+    ]
+    transforms = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            EnsureTyped(keys=["image", "label"]),
+            Orientationd(keys=["image", "label"], axcodes='IPL'),
+            Spacingd(keys=["image", "label"], pixdim=res),
+        ]
+    )
+
+    os.makedirs(out_dir, exist_ok=True)
+
+    dataset_json = json.load(open(os.path.join(dataset, 'dataset.json')))
+    labels_list = dataset_json['labels']
+    # in amos i had this issue that organ was the value?
+    labels_list = {v: int(k) for k, v in labels_list.items()} if list(labels_list.keys())[0].isdigit() else labels_list
+    save_json(out_dir, labels_list, len(images))
+
+    save_transform = Compose(
+        [
+            SaveImaged(
+                keys=["image"],
+                output_dir=os.path.join(out_dir, 'imagesTr'),
+                output_postfix="",
+                resample=False,
+                separate_folder=False,
+            ),
+            SaveImaged(
+                keys=["label"],
+                output_dir=os.path.join(out_dir, 'labelsTr'),
+                output_postfix="",
+                resample=False,
+                separate_folder=False,
+            ),
+        ]
+    )
+
+    dataset = monai.data.Dataset(data=val_files, transform=transforms)
+    #dataloader = monai.data.DataLoader(dataset, batch_size=1, num_workers=8)
+
+    for idx, data in enumerate(tqdm.tqdm(dataset, total=len(dataset))):
+        save_transform(data)
+
+        if idx==0:
+            img = data['image'].cpu().numpy().squeeze()
+            lab = data['label'].cpu().numpy().squeeze()
+            viz_mid_slices(img, lab, filename="test.png")
+
+
+def viz(dataset):
+    images, segs = find_and_sort_files(dataset)
+
+    val_files = [
+        {"image": img, "label": seg} for img, seg in zip(images, segs)
+    ]
+    transforms = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            EnsureTyped(keys=["image", "label"]),
+        ]
+    )
+
+    dataset_json = json.load(open(os.path.join(dataset, 'dataset.json')))
+    labels = dataset_json['labels']
+
+    dataset = monai.data.Dataset(data=val_files, transform=transforms)
+
+
+    os.makedirs('viz', exist_ok=True)
+    for idx, data in enumerate(tqdm.tqdm(dataset, total=len(dataset))):
+
+
+        img = data['image'].cpu().numpy().squeeze()
+        lab = data['label'].cpu().numpy().squeeze()
+        viz_mid_slices(img, lab, labels, filename=f"viz/test#{idx}.png")
+
+
+if __name__ == '__main__':
+    import fire;fire.Fire()
