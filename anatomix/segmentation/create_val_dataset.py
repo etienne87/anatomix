7d2b5da79613780d09890a9b38ebbe720d543711
eperot <etienne.perot@gehealthcare.com>
2025-12-11 18:07:25 +0100

    clean

---
+++ anatomix/segmentation/create_val_dataset.py
@@ -0,0 +1,51 @@
+"""
+Load relevant dataset in amos_pp
+"""
+import os
+import glob
+import json
+import numpy as np
+import common_io as cio
+import matplotlib.pyplot as plt
+
+
+def load_2d_seglists_cases():
+    dir_path = os.path.join("/net/frbucawnas02/export/hadokenalgo/projects/oneview_for_mr/gt/gt_2D_test/")
+    seglists = glob.glob(dir_path + '*.seglist')
+
+    baseline_mr_json = os.path.join("/home/eperot/nnUNet_raw/Dataset908_baselineMR/dataset.json")
+    mr = json.load(open(baseline_mr_json, 'r'))
+    labels = mr['labels']
+
+    case_slices = {}
+
+    cases_masks = {}
+    for seglist in seglists:
+        case_num = int(os.path.basename(seglist).split('_')[1].split('.nii.gz')[0])
+        label = os.path.basename(seglist).split('_')[-1].split('.seglist')[0]
+        if label in ['trabecularBone', 'ascite']:
+            continue
+        mask = cio.load_seg(seglist)>0
+
+        label_num = labels.get(label, 0)
+
+        img = cases_masks.get(case_num, np.zeros(mask.shape, dtype=np.int32))
+        img[mask] = label_num
+        cases_masks[case_num] = img
+
+
+        slice_num = np.stack(np.where(img>0), axis=-1)
+        slices = np.unique(slice_num[:,0])
+        case_slices[case_num] = slices
+        print(case_num, slices)
+
+        mip = img.max(axis=0)
+
+        plt.imsave(f"viz/test#{case_num}.png", mip)
+
+
+
+
+
+if __name__ == '__main__':
+    import fire;fire.Fire(load_2d_seglists_cases)
